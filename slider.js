aa5f984635fbe37e81506962e4de81073b7f0029
David Xiao <davidx@Huogens-MacBook-Pro.local>
2020-05-10 16:46:05 -0400

    animation

---
+++ slider.js
@@ -0,0 +1,33 @@
+let slider = document.getElementById("myRange"),
+    output = document.getElementById("display-value");
+
+output.innerHTML = slider.value;
+
+slider.max = branches.length - 1;
+
+
+slider.oninput = function() {
+  let ind = this.value;
+  output.innerHTML = ind;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i <= ind; i++) {
+    if (branches[i].length == 3) {
+      ctx.beginPath();
+      ctx.fillStyle = "white";
+      ctx.arc(branches[i][0], branches[i][1], 30 / branches[i][2], 0, 2 * Math.PI);
+      ctx.fill();
+      continue;
+    }
+
+    let x = branches[i][0],
+        y = branches[i][1],
+        nx = branches[i][2],
+        ny = branches[i][3],
+        depth = branches[i][4];
+
+    line(x, y, nx, ny, MAX_DEPTH - depth + 2);
+
+  }
+}
+
+console.log(branches.length);
